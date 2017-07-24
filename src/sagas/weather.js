2e9b04e28d2aa9f979d2e6df66e62bab256b8577
Casper Deseyne <casper.deseyne@gmail.com>
2017-07-24 09:40:55 +0200

    Update weather.js

--- src/sagas/weather.js
+++ src/sagas/weather.js
@@ -4,7 +4,6 @@ import { takeEvery } from 'redux-saga';
 import { fetchWeatherSuccess, fetchWeatherFail } from './../actions/weather';
 import actionTypes from './../actions';
 import axios from 'axios';
-import type WeatherStateType from './../reducers/weather';
 
 const weatherApiUrl = (location: string) => (
   `https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D"${location}")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`
