624b07bba18bab32f642be864046d9a608e4b37d
Casper Deseyne <casper.deseyne@gmail.com>
2017-07-24 10:54:10 +0200

    Improved saga

--- src/sagas/weather.js
+++ src/sagas/weather.js
@@ -9,17 +9,21 @@ const weatherApiUrl = (location: string) => (
   `https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D"${location}")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`
 );
 
-function* fetchWeather({location}: {location: string}) {
+const fetchWeather = (location: string) => {
+  return axios(weatherApiUrl(location)).then(({data:{query:{results:{channel:{item:{condition:{text}}}}}}}) => text);
+}
+
+function* callFetchWeather({location}: {location: string}) {
   try {
-    const {data: {query: {results: {channel: {item: {condition: { text }}}}}}} = yield axios.get(weatherApiUrl(location));
-    yield put(fetchWeatherSuccess(text));
-  } catch (error) {
-    yield put(fetchWeatherFail(error.message));
+    const result = yield call(fetchWeather, location);
+    yield put(fetchWeatherSuccess(result));
+  } catch ({message}) {
+    yield put(fetchWeatherFail(message));
   }
 }
 
 function* weatherSaga(): Generator<> {
-  yield takeEvery(actionTypes.WEATHER_FETCH_START, fetchWeather);
+  yield takeEvery(actionTypes.WEATHER_FETCH_START, callFetchWeather);
 }
 
 export default weatherSaga;
